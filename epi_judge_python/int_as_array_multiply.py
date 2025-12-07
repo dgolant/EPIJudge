2f239bbd2c13d8fe579db1a68861f1735bc41ba9
Dan Golant <dgolant@gmail.com>
2025-12-07 13:54:37 -0500

    string_integer_conversion

--- epi_judge_python/int_as_array_multiply.py
+++ epi_judge_python/int_as_array_multiply.py
@@ -2,7 +2,8 @@ from typing import List
 
 from test_framework import generic_test
 
-
+# [1,0] * [-2, 2] -> [-2, 2, 0]
+# [-3,5,1] * [-7 , 8 , 9 ] -> [2,7,6, 9, 3, 9]
 def multiply(num1: List[int], num2: List[int]) -> List[int]:
     # TODO - you fill in here.
     return []
--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -1,15 +1,69 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
+string_int_map = {
+    "0": 0,
+    "1": 1, 
+    "2": 2,
+    "3": 3, 
+    "4": 4,
+    "5": 5, 
+    "6": 6,
+    "7": 7, 
+    "8": 8,
+    "9": 9, 
+    "-": -1,
+}
+
+int_string_map = {
+    0: "0",
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4",
+    5: "5",
+    6: "6",
+    7: "7",
+    8: "8",
+    9: "9",
+    -1: "-",
+}
+
 
 def int_to_string(x: int) -> str:
-    # TODO - you fill in here.
-    return '0'
+    if x == 0:
+        return "0"
+    
+    input = abs(x)
+    output = []
+    while input > 0:
+      lsb = input%10
+      output.append(int_string_map[lsb])
+      # drop the least significant digit
+      input//=10
+    if x < 0:
+        # swap the final digit magnitude
+        output[-1]= "-"+output[-1]
+    res = "".join(reversed(output))
+    return res
+
 
 
 def string_to_int(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    res=0
+    mag=1
+    for c in reversed(s):
+        if c == "+":
+            continue
+        i = string_int_map[c]
+        if i != -1:
+          adder = i*mag
+          res+=adder
+        else:
+            # we know we're on the last index so this is safe
+            res*=-1
+        mag*=10
+    return res
 
 
 def wrapper(x, s):
