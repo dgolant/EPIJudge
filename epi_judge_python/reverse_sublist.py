00c120efbbe6318cb9563bffc6f4ee26e6c50461
Dan Golant <daniel.golant@datadoghq.com>
2026-01-05 18:31:19 -0500

    reverse_sublist (fail)

--- epi_judge_python/reverse_sublist.py
+++ epi_judge_python/reverse_sublist.py
@@ -3,12 +3,25 @@ from typing import Optional
 from list_node import ListNode
 from test_framework import generic_test
 
+def traverse_until(L: ListNode, target: int):
+    for i in range(target-2):
+        L = L.next
+    return L
 
 def reverse_sublist(L: ListNode, start: int,
                     finish: int) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    dummy_head = sublist_head = ListNode(0, L)
+    # start is 1-based, and we go to the node right before start
+    for _ in range(1, start):
+        sublist_head = sublist_head.next
 
+
+    sublist_iter = sublist_head.next
+    for _ in range(finish-start):
+        temp = sublist_iter.next
+        sublist_iter.next, temp.next, sublist_head.next = (temp.next, sublist_head.next, temp)
+
+    return dummy_head.next
 
 if __name__ == '__main__':
     exit(
