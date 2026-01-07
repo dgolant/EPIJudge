01a825c3b22a8fbdfe83894fa283e9d7417f4f36
Dan Golant <daniel.golant@datadoghq.com>
2026-01-06 21:29:50 -0500

    stack_with_max

--- epi_judge_python/stack_with_max.py
+++ epi_judge_python/stack_with_max.py
@@ -3,20 +3,27 @@ from test_framework.test_failure import TestFailure
 
 
 class Stack:
+
+    def __init__(self):
+        self.data = []
+        self.max_cache = [-float("inf")]
     def empty(self) -> bool:
-        # TODO - you fill in here.
-        return True
+        return len(self.data) == 0
 
     def max(self) -> int:
         # TODO - you fill in here.
-        return 0
+        return self.max_cache[-1]
 
     def pop(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        val = self.data.pop()
+        if val == self.max_cache[-1]:
+            self.max_cache.pop()
+        return val
 
     def push(self, x: int) -> None:
-        # TODO - you fill in here.
+        if x > self.max_cache[-1]:
+            self.max_cache.append(x)
+        self.data.append(x)
         return
 
 
