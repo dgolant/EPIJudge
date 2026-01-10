0bb952ef1fa7664bf6ecd42e4ce6b777e95b2657
Dan Golant <daniel.golant@datadoghq.com>
2026-01-10 01:12:46 -0500

    Evaluate RPN

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -1,9 +1,34 @@
 from test_framework import generic_test
+from collections import deque
 
+operators = set(["+", "-", "*", "/"])
+
+def flush(operator, first, second):
+    match operator:
+        case "+":
+            return int(first)+int(second)
+        case "-":
+            return int(first)-int(second)
+        case "*":
+            return int(first)*int(second)
+        case "/":
+            return int(first)/int(second)
 
 def evaluate(expression: str) -> int:
     # TODO - you fill in here.
-    return 0
+    ops = deque(expression.split(","))
+    print(expression)
+    expr = []
+    while len(ops):
+        operand = ops.popleft()
+        if operand in operators:
+            second, first = expr.pop(), expr.pop()
+            product = flush(operand, first, second)
+            ops.appendleft(product)
+        else:
+            expr.append(operand)
+
+    return int(expr[0])
 
 
 if __name__ == '__main__':
