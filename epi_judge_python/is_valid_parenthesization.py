858875f453c2b73640ccc0057c1d7fac4f692968
Dan Golant <dgolant@gmail.com>
2026-01-11 00:26:59 -0500

    is_valid_parenthization

--- epi_judge_python/is_valid_parenthesization.py
+++ epi_judge_python/is_valid_parenthesization.py
@@ -1,9 +1,26 @@
 from test_framework import generic_test
 
 
+matches = {
+  "]": "[",
+  "}": "{",
+  ")": "("
+}
+
 def is_well_formed(s: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    stack = []
+    for char in s:
+        if char in ["{", "[", "("]:
+            stack.append(char)
+        else:
+            if not stack:
+                return False
+            last = stack.pop()
+            if char in matches and matches[char] == last:
+                pass
+            else:
+                return False
+    return len(stack) == 0
 
 
 if __name__ == '__main__':
