032b0792b7618a73d8d250730f853367ce34b46d
Dan Golant <dgolant@gmail.com>
2026-01-11 10:41:10 -0500

    directory_path_normalization

--- epi_judge_python/directory_path_normalization.py
+++ epi_judge_python/directory_path_normalization.py
@@ -2,8 +2,28 @@ from test_framework import generic_test
 
 
 def shortest_equivalent_path(path: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    
+    if not path:
+        raise ValueError("empty path")
+    
+    stack = []
+    if path[0] == "/":
+        stack.append("/")
+        
+    
+    for token in (token for token in path.split("/") if token not in [".", ""]):
+        if token == "..":
+          if not stack or stack[-1] == token:
+            stack.append(token)
+          else:
+            if stack[-1] == "/":
+               raise ValueError("boo")
+            stack.pop()
+        else:
+            stack.append(token)
+
+    res = "/".join(stack)
+    return res[res.startswith("//"):]
 
 
 if __name__ == '__main__':
