201b2fccffd6cd1724face7860c6608d66c37014
Dan Golant <dgolant@gmail.com>
2026-01-11 13:25:18 -0500

    is_tree_balanced

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -1,10 +1,23 @@
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
+def is_balanced_inner(node: BinaryTreeNode) -> int | bool:
+    if not node:
+        return -1, True
+    left_height, left_bal = is_balanced_inner(node.left)
+    right_height, right_bal = is_balanced_inner(node.right)
+    height = max(left_height, right_height)+1
+    balanced = left_bal and right_bal and abs(left_height-right_height) <=1
+    return height, balanced
 
 def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    if not tree:
+        return True
+    left_height, left_balance = is_balanced_inner(tree.left)
+    right_height, right_balance = is_balanced_inner(tree.right)
+    if not left_balance or not right_balance:
+        return False
+    return abs(left_height-right_height) <= 1
 
 
 if __name__ == '__main__':
