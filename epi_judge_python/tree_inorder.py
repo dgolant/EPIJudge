955e95beb99617bcb97dfdb4d2ab059e63ae4232
Dan Golant <dgolant@gmail.com>
2026-01-16 23:41:54 -0500

    tree_inorder

--- epi_judge_python/tree_inorder.py
+++ epi_judge_python/tree_inorder.py
@@ -5,8 +5,23 @@ from test_framework import generic_test
 
 
 def inorder_traversal(tree: BinaryTreeNode) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    if not tree:
+        return []
+    stack = [(tree, False)]
+    res = []
+    while stack:
+        node, processed = stack.pop()
+        if processed:
+          res.append(node.data)
+        else:
+            # right, curr, left (so that we keep traversing left when we next pop)
+            if node.right:
+                stack.append((node.right, False))
+            stack.append((node, True))
+            if node.left:
+                stack.append((node.left, False))
+
+    return res
 
 
 if __name__ == '__main__':
