74217d2562ba5d7d955bcbf995d0498a9bc59e71
Dan Golant <dgolant@gmail.com>
2026-01-18 20:28:30 -0500

    sort_almost_sorted

--- epi_judge_python/sort_almost_sorted_array.py
+++ epi_judge_python/sort_almost_sorted_array.py
@@ -1,12 +1,25 @@
 from typing import Iterator, List
 
 from test_framework import generic_test
-
+import heapq
 
 def sort_approximately_sorted_array(sequence: Iterator[int],
                                     k: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    
+    heap = []
+    res = []
+    # Prefill the heap with k instances, since that's the max range we'll ever have to sort
+    for i in range(k):
+        num = next(sequence)
+        heapq.heappush(heap, num)
+
+    # then, keep pushing, and evict the smallest number each time. push each evicted number into the result
+    for num in sequence:
+        res.append(heapq.heappushpop(heap, num))
+    # we may have to pop k times at the end too
+    while heap:
+        res.append(heapq.heappop(heap))
+    return res
 
 
 def sort_approximately_sorted_array_wrapper(sequence, k):
