972d82653dca901a830576b7ca3366756cecd77a
Dan Golant <daniel.golant@datadoghq.com>
2026-01-24 19:49:35 -0500

    add first k

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -1,11 +1,23 @@
 from typing import List
 
 from test_framework import generic_test
-
+import bisect
 
 def search_first_of_k(A: List[int], k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    l = 0
+    h = len(A)-1
+    first_k = -1
+
+    while l <= h:
+        midpoint = l+((h-l)//2)
+        if A[midpoint]==k:
+            first_k = midpoint
+            h=midpoint-1
+        elif k > A[midpoint]:
+            l = midpoint+1
+        elif k < A[midpoint]:
+            h = midpoint-1
+    return first_k
 
 
 if __name__ == '__main__':
