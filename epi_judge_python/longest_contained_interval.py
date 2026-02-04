84299bbeb47e403b1fb31f8c52b140599aede5be
Dan Golant <daniel.golant@datadoghq.com>
2026-02-04 13:58:36 -0500

    longest contained interval

--- epi_judge_python/longest_contained_interval.py
+++ epi_judge_python/longest_contained_interval.py
@@ -4,8 +4,24 @@ from test_framework import generic_test
 
 
 def longest_contained_range(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    presence = {}
+    min_val = float("inf")
+    max_val = -float("inf")
+    max_run = 0
+    # Build a dictionary tracking which keys are present, and the minimum and maximum values
+    for val in A:
+        presence[val] = True
+        min_val = min(min_val, val)
+        max_val = max(max_val, val)
+
+    run = 0
+    for i in range(min_val, max_val+1):
+        if i not in presence:
+            run = 0
+        else:
+            run+=1
+        max_run = max(max_run, run)
+    return max_run
 
 
 if __name__ == '__main__':
