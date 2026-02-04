e5e82848786b31f7bf7ac3004e01754f4109a31a
Dan Golant <daniel.golant@datadoghq.com>
2026-02-04 13:36:59 -0500

    Longest subarray iwth distinct values

--- epi_judge_python/longest_subarray_with_distinct_values.py
+++ epi_judge_python/longest_subarray_with_distinct_values.py
@@ -4,8 +4,23 @@ from test_framework import generic_test
 
 
 def longest_subarray_with_distinct_entries(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    l, r = 0, 0
+    max_len = 0
+    positions = {}
+    while r < len(A):
+        char = A[r]
+
+        if char in positions and positions[char] >= l:
+            l = positions[char]+1
+
+        positions[char] = r
+
+
+        max_len = max(max_len, (r - l)+1)
+        r+=1
+    return max_len
+
+
 
 
 if __name__ == '__main__':
